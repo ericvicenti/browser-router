bc5075007a51a0c2547731d6ded56b8b962c4cb4
Eric Vicenti <ericvicenti@gmail.com>
2014-01-27 23:02:16 -0800

    progress

--- browser-router.js
+++ browser-router.js
@@ -1,15 +1,16 @@
 var queryString = require('query-string');
 var History = require('html5-history');
 var _ = require('underscore');
+var window = require('globals/window');
 
 var state = {};
 
 var router = exports;
 
 router.route = function route(path, data, title) {
-  state.path = path || router.getPath();
-  state.data = data || {};
-  state.title = title || '';
+  state.path = path || state.path || router.getPath();
+  state.data = data || state.data || {};
+  state.title = title || state.title || '';
   var search = '';
   if (!_.isEmpty(state.data)) {
     search = '?' + queryString.stringify(state.data)
@@ -17,8 +18,24 @@ router.route = function route(path, data, title) {
   History.pushState(state.data, state.title, state.path + search);
 }
 
+
+var currentHandlers = [];
+
+History.Adapter.bind(window, 'statechange', function() {
+  state.path = router.getPath();
+  state.params = router.getParams();
+  state.title = router.getTitle();
+  _.each(currentHandlers, function(handler) {
+    handler(state.path, state.params, state.title);
+  });
+});
+
 router.onRoute = function(handler) {
-  History.Adapter.bind(window,'statechange',handler);
+  currentHandlers = _.union(currentHandlers, handler);
+}
+
+router.offRoute = function(handler) {
+  currentHandlers = _.without(currentHandlers, handler);
 }
 
 router.getPath = function getPath() {
@@ -26,9 +43,17 @@ router.getPath = function getPath() {
   return path;
 }
 
+router.setPath = function setPath(path) {
+  router.route(path, state.params, state.title);
+}
+
 router.getTitle = function getTitle() {
   var title = window.document.title;
   return title;
+}
+
+router.setTitle = function setTitle(title) {
+  window.document.title = state.title = title;
 }
 
 router.getParams = function getParams() {
@@ -64,6 +89,14 @@ router.removeParam = function removeParam(paramName) {
   var params = router.getParams();
   delete params[paramName];
   router.setParams(params);
+}
+
+router.toggleParam = function toggleParam(paramName, value) {
+  if (router.hasParam(paramName)) {
+    router.removeParam(paramName);
+  } else {
+    router.setParam(paramName, value);
+  }
 }
 
 router.back = function back() {
